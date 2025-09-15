a9e0028b83e05a5254a814c187c044ba88d272ac
SIAM_MHORIZON\siamm <siam.mhorizon@gmail.com>
2025-09-14 18:37:57 -0700

    - Added validation of arguments, and proper usage message

--- TMMCLineCounterApp/Program.cs
+++ TMMCLineCounterApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TMMCLineCounterApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid input. Please Enter: TMMCLineCounterApp.exe <image-path>");
+                return 1;
+            }
+
+            string imgPath = args[0];
+            Console.WriteLine($"Argument received: {imgPath}");
+            return 0;
+        }
+    }
+}
